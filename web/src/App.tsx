0172602b1cfbdc060896a083344996c5b32c8c59
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-03-01 03:51:21 -0300

    feat: tailwind prettier configured

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import './global.css'
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <h1>Hello Pizza</h1>
       <Button>Enviar</Button>
     </div>
