4b6fbc42a3b3d02ec0622415f6203dcbb4080496
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-03-01 04:04:05 -0300

    feat: router configured

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,10 @@
 import './global.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>Hello Pizza</h1>
-      <Button>Enviar</Button>
-    </div>
+    <RouterProvider router={router} />
   )
 }
