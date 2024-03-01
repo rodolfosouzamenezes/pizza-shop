809a42aa04a39ac71c5009ccb67d35c2faec63e4
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-03-01 03:38:37 -0300

    feat: configured shadcn/ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
-    <>
+    <div className='flex justify-center items-center h-screen'>
       <h1>Hello Pizza</h1>
-    </>
+      <Button>Enviar</Button>
+    </div>
   )
 }
 
