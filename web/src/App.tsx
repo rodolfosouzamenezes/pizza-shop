f75f53f1495bb1efc8cecda286137fc8cba0323f
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-03-04 04:12:29 -0300

    feat: add theme toggle

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors duration={1500} />
+      <ThemeProvider storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors duration={1500} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
