3ea7400098b2289359f8202d34fb3be61d464e2f
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2024-03-05 04:42:19 -0300

    feat: add order details dialog

--- web/src/pages/app/orders/order-table-row.tsx
+++ web/src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,24 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do Pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do Pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="tetx-xm font-mono font-medium uppercase">
         9089dnuge
@@ -19,7 +27,7 @@ export function OrderTableRow() {
       <TableCell>
         <div className="flex items-center gap-2">
           <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span>Pendente</span>
+          <span className="font-medium text-muted-foreground">Pendente</span>
         </div>
       </TableCell>
       <TableCell className="font-medium">Lucas Andrade Fernandes</TableCell>
